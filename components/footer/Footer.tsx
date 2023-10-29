63c9f484c762cfdbb6f0ef49f83ba49f8784fe45
pantazisoftware <robert.pantazi02@gmail.com>
2023-10-29 23:42:45 +0200

    second commit

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -5,7 +5,12 @@ const Footer = () => {
       <div className="flex flex-col items-start justify-between mt-10 border-t border-gray-200 py-6 content">
         <div id="logo">
           <Logo />
-          <p className="text-gray-500 text-sm">Digital Solutions, Infinite Possibilities</p>
+          <p className="text-gray-500 text-sm">
+            Digital Solutions, Infinite Possibilities
+          </p>
+          <p>
+            Phone <a href="tel://+40751057981">+40 751 057 981</a>
+          </p>
         </div>
       </div>
     );
