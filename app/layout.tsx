8d55094aecb767103e27d0f919a3bac57abd160d
pantazisoftware <robert.pantazi02@gmail.com>
2024-07-06 15:56:26 +0300

    vercel analytics done

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} font-sans antialiased text-gray-800`}>
+        <Analytics />
         <Navbar />
         {children}
         <Footer />
